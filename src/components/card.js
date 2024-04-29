65cbf6cb6cc523494559ebb07a4a14dfaaafc733
Alex <rossetka@mail.ru>
2024-04-29 23:20:56 +0300

    making edits

--- src/components/card.js
+++ src/components/card.js
@@ -1,49 +1,37 @@
 const cardTemplate = document.querySelector("#card-template").content;
-
+ 
 // Функция создания карточки
 export function createCard(information, removeCard, toggleLike, enlargeImage) {
 const card = cardTemplate.querySelector(".card").cloneNode(true);
 const cardDeleteButton = card.querySelector(".card__delete-button");
 const cardLikeButton = card.querySelector(".card__like-button");
 const cardImage = card.querySelector(".card__image");
-
+ 
 card.querySelector(".card__title").textContent = information.name;
 cardImage.src = information.link;
 cardImage.alt = `Изображение ${information.name}`;
-
+ 
 cardDeleteButton.addEventListener("click", function () {
 removeCard(card);
 });
-
+ 
 cardLikeButton.addEventListener("click", function () {
 toggleLike(cardLikeButton);
 });
-
+ 
 cardImage.addEventListener("click", function () {
 enlargeImage(information);
 });
-
+ 
 return card;
 }
-
+ 
 // Функция удаления карточки
 export function removeCard(card) {
 card.remove();
 }
-
+ 
 // Функция обработки событий лайка
 export function toggleLike(buttonElement) {
 buttonElement.classList.toggle("card__like-button_is-active");
-}
-
-// Открытие попапа с картинкой
-export function enlargeImage(image, altText, popupImage) {
-  const popupImageElement = popupImage.querySelector(".popup__image");
-  const popupCaption = popupImage.querySelector(".popup__caption");
-
-  popupImageElement.src = image;
-  popupImageElement.alt = altText;
-  popupCaption.textContent = altText;
-
-  openModal(popupImage);
 }
